823cf45457a46dc79aeded97210f53a21b29fd0e
thelegendarysanin <abhishek501357@gmail.com>
2020-06-25 13:20:03 +0530

    Initial Launch page

---
+++ ReadWrite.py
@@ -0,0 +1,5 @@
+file = open("E:/abhi/Python Udemy codes/My Codes/File.txt")
+
+print(file.read(10))
+
+file.close()
---
+++ SeleniumFirst.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+
+# driver = webdriver.Chrome(executable_path='E:/abhi/driver/chromedriver.exe')
+# driver.get("https://www.google.com/")
+# driver.close()
+
+list1= [1,2,3,4]
+
+# print(list(reversed(list1)))
+
+for i in reversed(list1):
+    print(i)
